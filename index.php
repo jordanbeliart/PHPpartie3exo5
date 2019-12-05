e630920d3b70b4bd1007fd3bc95279abfa6d8737
jordanbeliart <j.beliart.jb@gmail.com>
2019-12-05 09:56:26 +0100

    correction

--- index.php
+++ index.php
@@ -1,22 +1,20 @@
 <?php
 $departments = array(
-  02 => 'Aisne',
-  59 => 'Nord',
-  60 => 'Oise',
-  62 => 'Pas-de-Calais',
-  80 => 'Somme');
+  '02' => 'Aisne',
+  '59' => 'Nord',
+  '60' => 'Oise',
+  '62' => 'Pas-de-Calais',
+  '80' => 'Somme');
 ?>
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
-  <head>
-    <meta charset="utf-8" />
-    <title>Php partie 3 exo5</title>
-  </head>
-  <body>
-    <?php
-      foreach ($departments as $value) {
-        echo $value . '<br />';
-      }
-    ?>
-  </body>
+<head>
+  <meta charset="utf-8" />
+  <title>Php partie 3 exo5</title>
+</head>
+<body>
+  <?php foreach ($departments as $value) { ?>
+    <p><?php echo $value; ?></p>
+  <?php }; ?>
+</body>
 </html>
